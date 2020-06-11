478d8b7d85e9e9eb5b7c5d1f05292d96d8ee8925
Khalil <49612383+khalilVer@users.noreply.github.com>
2020-06-11 13:52:39 +0100

    adjust template

---
+++ src/app/admin-office-sidebar/admin-office-sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-office-sidebar',
+  templateUrl: './admin-office-sidebar.component.html',
+  styleUrls: ['./admin-office-sidebar.component.css']
+})
+export class AdminOfficeSidebarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
