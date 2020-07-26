d922c9f38b4e5c5d35150a633cc55a6a6d153997
ayaGharbi <gharbiaya137@gmail.com>
2020-07-27 01:11:22 +0200

    Update sell.component.ts

--- src/app/sell/sell.component.ts
+++ src/app/sell/sell.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {DemandeService} from '../service/demande.service';
+import {Router} from '@angular/router';
+import {Sell} from '../Model/sell';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-sell',
@@ -6,10 +12,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sell.component.css']
 })
 export class SellComponent implements OnInit {
+  constructor(private offreService: DemandeService, public router: Router) { }
+  Offres: any = [];
+  categorie: string;
+  b: Array<Sell> = [];
 
-  constructor() { }
 
   ngOnInit() {
+    this.loadOffres();
+  }
+  // Get products list
+  loadOffres() {
+    return this.offreService.getProducts().subscribe((data: {}) => {
+      this.Offres = data;
+    });
+  }
+  deleteOffre(id) {
+    if (window.confirm('Are you sure, you want to delete?')) {
+      this.offreService.deleteProduct(id).subscribe(data => {
+        this.loadOffres();
+      });
+    }
+  }
+
+  searchByCategorie(categorie) {
+    this.offreService.getByCategorie(categorie).subscribe(data => {
+      this.loadOffres();
+    });  }
+  generatePdf(offres) {
+    const documentDefinition = {content: 'Votre commande est bien enregitré! Merci pour votre confiance'};
+    if (window.confirm('Voulez-vous confimer votre achat? ')) {
+      pdfMake.createPdf(documentDefinition).download();
+    }
+    for (let i = 0; i < this.b.length; i++) {
+      this.b[i].etat = 'Acheté' ;
+    }
   }
 
 }
